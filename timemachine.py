88ce22817b09898af1913aa5dc8bf7d05b6dc1cc
Denniskamau <denniskamau3@gmail.com>
2017-11-29 12:56:50 +0300

    Add a command line options

--- timemachine.py
+++ timemachine.py
@@ -1,7 +1,20 @@
 import yaml
+import argparse
 import logging
 
 from logging.handlers import RotatingFileHandler
+
+
+parser = argparse.ArgumentParser(description="Create copies of specified files and watch for changes ")
+parser.add_argument('-a' ,'--add',default=None,action="store_true",
+                    help='Add a file to the list of files to be observed')
+
+parser.add_argument('-r' , '--remove',default=None,action="store_true",
+                    help='Remove a file from the list of files to be observed')
+
+parser.add_argument('-l','--list', action="store_true",
+                    help='Print a list of the file being observed')
+
 
 config = './config.dat'
 log = './logfile.log'
@@ -13,7 +26,15 @@ logger = logging.getLogger('dante')
 logger.setLevel(logging.DEBUG)
 logger.addHandler(rotating_handler)
 
+"""
+-function to read the config file data
+-Gives the user an option to give the location of the config file if none is provided then the default value of 
+config file in ./config.dat
+-Gives the user an option to give the location of where the files are to be copied to ,if none is given then the
+default location will be in the copies Directory
 
+-Copies the content of the file given in the config.dat to a file in the copies Directory
+"""
 def read_config_file():
     config_Location = input('Please input the location of the config file to read from? ')
 
@@ -91,6 +112,10 @@ def read_config_file():
             logger.debug('specified file location was incorrect')
             print('There was a problem loading config file, It appears the path specified is not correct!!')
 
+
+"""
+-A function to watch for changes in the original file and compare them to the copies created.
+"""
    
 
 def check_for_change():
@@ -101,6 +126,42 @@ def check_for_change():
         file2 = yaml.load(c)
         print(file2)
 
+
+# Check for all the files in config.dat
+def list_all_files():
+    print('checking for files')
+    try:
+        with open(config, 'r') as f:
+            files = yaml.load(f)
+            print (files)
+    except FileNotFoundError:
+        print("Config file {0} not found".format(filename), file=sys.stderr)
+        sys.exit(1)
+
+#Add a file to config.dat
+def add_file_to_config():
+    print('adding file to list of observable files')
+
+#Delete a file from config.dat
+def delete_file_from_config():
+     print('removing file from list of observable files')
     
-execute = read_config_file()
+
+
+
+
+args =parser.parse_args() 
+if args.list:
+    list_all_files()
+elif args.add:
+    add_file_to_config()
+elif args.remove:
+    delete_file_from_config()
+else:
+    execute = read_config_file()
+
+    
+
+
+
 #execute2 = checkForChange()
